94eb99fa85778d6f708ddf7a20064a222051c51f
BilionBoy <dhiogoandre26@gmail.com>
2025-02-25 19:29:14 -0400

    styles: tipografia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,12 @@ class ApplicationController < ActionController::Base
 
   def set_layout_by_controller
     self.class.layout case controller_name
-                      when "home"
-                        "application" # Aplica home_application para HomeController
-                      when "landing"
-                        "home_application" # Aplica home_application para LandingController
-                      else
-                        devise_controller? ? "devise_application" : "application"
-                      end
+     when "home"
+      "application" # Aplica home_application para HomeController
+     when "landing"
+      "home_application" # Aplica home_application para LandingController
+     else
+    devise_controller? ? "devise_application" : "application"
+      end
   end
 end
